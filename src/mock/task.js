ce1af2f4daa826ef95e9ad3581ff2979c24d624b
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-04-09 23:38:22 +0300

    Генерирование моков для task.

--- src/mock/task.js
+++ src/mock/task.js
@@ -1,4 +1,18 @@
-const DefaultRepeatingDays = {
+import {COLORS} from "../const.js";
+import {getRandomArrayElement, getRandomDate, generateBoolean} from "../random.js";
+
+const MIN_DATE_RANGE = 0;
+const MAX_DATE_RANGE = 8;
+
+const DESCRIPTION_ITEMS = [
+  `Изучить теорию`,
+  `Сделать домашку`,
+  `Пройти интенсив на соточку`,
+  `Помыть посуду`,
+  `Покормить кота`
+];
+
+const DEFAULT_REPEATING_DAYS = {
   "mo": false,
   "tu": false,
   "we": false,
@@ -8,21 +22,29 @@ const DefaultRepeatingDays = {
   "su": false,
 };
 
+const generateRepeatingDays = () => {
+  return Object.assign({}, DEFAULT_REPEATING_DAYS, {
+    "mo": generateBoolean()
+  });
+};
+
 const generateTask = () => {
+  const dueDate = generateBoolean() ? getRandomDate(MIN_DATE_RANGE, MAX_DATE_RANGE) : null;
+
   return {
-    description: `Example task with default color.`,
-    dueDate: Math.random() > 0.5 ? new Date() : null,
-    repeatingDays: Object.assign({}, DefaultRepeatingDays, {"mo": Math.random() > 0.5}),
-    color: `pink`,
-    isArchive: Math.random() > 0.5,
-    isFavorite: Math.random() > 0.5
+    description: getRandomArrayElement(DESCRIPTION_ITEMS),
+    dueDate,
+    repeatingDays: dueDate ? generateRepeatingDays() : DEFAULT_REPEATING_DAYS,
+    color: getRandomArrayElement(COLORS),
+    isArchive: generateBoolean(),
+    isFavorite: generateBoolean()
   };
 };
 
 const generateTasks = (count) => {
   return new Array(count)
-            .fill(``)
-            .map(generateTask);
+    .fill(``)
+    .map(generateTask);
 };
 
 
