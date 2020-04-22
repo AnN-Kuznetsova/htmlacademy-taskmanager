88c495e296e4c2b05683c146e3ebd0dcc17f3dfd
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-04-22 23:01:11 +0300

    Создаем функцию `remove` для для удаления DOM-элемента.

--- src/utils/render.js
+++ src/utils/render.js
@@ -3,6 +3,13 @@ const RenderPosition = {
   BEFOREEND: `beforeend`
 };
 
+const createElement = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+
+  return newElement.firstChild;
+};
+
 const render = (container, element, place) => {
   switch (place) {
     case RenderPosition.AFTERBEGIN:
@@ -15,21 +22,19 @@ const render = (container, element, place) => {
   }
 };
 
-const createElement = (template) => {
-  const newElement = document.createElement(`div`);
-  newElement.innerHTML = template;
-
-  return newElement.firstChild;
-};
-
 const replace = (parent, newElement, oldElement) => {
   parent.replaceChild(newElement, oldElement);
 };
 
+const remove = (element) => {
+  element.remove();
+};
+
 
 export {
+  createElement,
   RenderPosition,
   render,
   replace,
-  createElement,
+  remove,
 };
