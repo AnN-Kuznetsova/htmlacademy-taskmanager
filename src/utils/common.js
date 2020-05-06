c40ad8ee1ac23e02400928106cf27f31f35288b7
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-05-07 02:01:30 +0300

    Удаляем лишнее.

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,20 +1,5 @@
 import moment from "moment";
 
-const MONTH_NAMES = [
-  `January`,
-  `February`,
-  `March`,
-  `April`,
-  `May`,
-  `June`,
-  `July`,
-  `August`,
-  `September`,
-  `October`,
-  `November`,
-  `December`,
-];
-
 
 const formatDate = (date) => {
   return moment(date).format(`DD MMMM`);
