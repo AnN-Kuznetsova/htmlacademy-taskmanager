49583784b129aba095f32c52f3aa25b75e164467
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-05-16 02:00:42 +0300

    Исправляем ошибку обновления данных.

--- src/controllers/task-controller.js
+++ src/controllers/task-controller.js
@@ -54,7 +54,7 @@ export default class TaskController {
 
     return new TaskModel({
       "description": formData.get(`text`),
-      "dueDate": date ? new Date(date) : null,
+      "due_date": date ? new Date(date) : null,
       "repeating_days": formData.getAll(`repeat`).reduce((acc, it) => {
         acc[it] = true;
         return acc;
